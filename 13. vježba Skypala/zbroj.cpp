9a2d76bc0836d0f222d060ab2f8d679e93cc86b1
Skyfall33 <otto.skypala@gmail.com>
2017-05-15 09:28:47 +0200

    ispisuje se glavni izbornik

--- 13. vježba Skypala/zbroj.cpp
+++ 13. vježba Skypala/zbroj.cpp
@@ -3,7 +3,12 @@ using namespace std;
 
 int main()
 {
-    int a, b;
-    cin >> a >> b;
-    cout << a + b << endl;
+    cout << "Glavni izbornik" << endl;
+    cout << "1. Unos nove osobe" << endl;
+    cout << "2. Ispisi podatke" << endl;
+    cout << "3. pretraga prema racunu" << endl;
+    cout << "4. Pretraga prema prezimenu" << endl;
+    cout << "5. Ispisi sortirano" << endl;
+    int izbor;
+    cin >> izbor;
 }
